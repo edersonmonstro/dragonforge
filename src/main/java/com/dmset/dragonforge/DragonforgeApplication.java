5fb78842b01b1871cce553a993cc04f0874819b5
ederson <edersoncolares@defensoria.am.def.br>
2024-06-11 08:30:04 -0400

    first commit. just the structure from initialr

---
+++ src/main/java/com/dmset/dragonforge/DragonforgeApplication.java
@@ -0,0 +1,13 @@
+package com.dmset.dragonforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonforgeApplication.class, args);
+	}
+
+}
